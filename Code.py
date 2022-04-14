3f7af0103249dfbfe56ab4586d2a6495e10164d7
Yashika-Kadiyan <103628717+Yashika-Kadiyan@users.noreply.github.com>
2022-04-14 14:33:24 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,33 +1,34 @@
 print('Hello')
-ans=input('Are you ready to play (yes/no):')
+A=input('Are you ready to play (yes/no):')
 score=0
 total_q=4
-if ans.lower() == 'yes':
-    ans=input('1. What is the best programming language ?')
-    if ans.lower() == 'python':
+if A == 'yes':
+    print('All the best for this Quiz')
+    A=input('1. Python is a programming language or not ?')
+    if A == 'yes':
         score+=1 
         print('correct')
     else :
         print('incorrect')
-    ans=input('2. What is 2+8+9-1 ?')
-    if ans == '18':
+    A=input('2. What is 2+8+9-1 ?')
+    if A == '18':
         score+=1 
         print('correct')
     else :
         print('incorrect')
-    ans=input('3. What is the better 1050ti or a 1060 (graphics card) ?')
-    if ans.lower() == '1050ti':
+    A=input('3. How many operators are in Python ?')
+    if A == '3':
         score+=1 
         print('correct')
     else :
         print('incorrect')
-    ans=input('4. Who came second in the stanely cup finals ?')
-    if ans.lower() == 'nights' or ans.lower() == 'vegas':
+    A=input('4. Which oprator have higher precedence in Python?')
+    if A == 'arithmetic operator':
         score+=1 
         print('correct')
     else :
         print('incorrect')
 print('Thank you for playing ,you got',score,"question correct.")
-mark=(score/total_q)*100 
-print("Mark:",mark)
+marks=(score/total_q)*100 
+print("Marks:",marks)
 print('Goodbye')
